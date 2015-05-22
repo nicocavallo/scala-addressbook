2dac108b82bcd16a03227d72383365d8ccc12ac7
ncavallo <nicolas.cavallo@gmail.com>
2015-05-22 15:22:51 -0300

    A few optimizations regarding the complexity of reading data.

--- src/main/scala/gumtree/nicocavallo/GumtreeCodingChallenge.scala
+++ src/main/scala/gumtree/nicocavallo/GumtreeCodingChallenge.scala
@@ -7,16 +7,25 @@ import scala.util.Try
 /**
  * Created by ncavallo on 22/05/15.
  */
-trait GumtreeCodingChallenge { self: AddressBookReader =>
+trait GumtreeCodingChallenge {
+  self: AddressBookReader =>
 
+  /**
+   * Helper storage for optimizing count by gender
+   */
   private lazy val byGender = addresses.groupBy[String](c => c.gender).mapValues(_.size)
+
+  /**
+   * Another helper storage for optimizing access to the users by name.
+   */
+  private lazy val byName = addresses.groupBy(_.name)
 
   /**
    * Counts how many males are in the Address Book
    * @return number of males in the Address Book, 0 if it is empty
    */
   def countMales = {
-   countByGender("Male")
+    countByGender("Male")
   }
 
   private def countByGender(gender: String) = byGender.get(gender).getOrElse(0)
@@ -25,7 +34,7 @@ trait GumtreeCodingChallenge { self: AddressBookReader =>
    * This method returns the oldest contact (if Address Book is not empty).
    * @return an Option: 'Some oldest contact' or 'None' if the Address Book is empty
    */
-  def oldest:Option[Contact] = {
+  def oldest: Option[Contact] = {
     Try(addresses.minBy(_.birthDay)) map { c =>
       Some(c)
     } recover {
@@ -36,23 +45,23 @@ trait GumtreeCodingChallenge { self: AddressBookReader =>
 
   /**
    *
-   * @param fromName - Oldest person's name to compare
-   * @param toName - Youngest person's name to compare
+   * @param fromName - Oldest person's name to compare. Should not be null
+   * @param toName - Youngest person's name to compare. Should not be null
    * @return an Option:
    *         - Some(days) where days can be 0 if 'from' and 'to' are the same person or were born in the same day
-   *         - None if any or both of them does not exist in the AddressBook
+   *         - None if any or both of them does not exist or if there is a duplicated name in the AddressBook
    */
-  def daysDiff(fromName: String, toName: String):Option[Long] = {
-
+  def daysDiff(fromName: String, toName: String): Option[Long] = {
     require(fromName != null && toName != null)
 
-    val fromOpt = addresses.find(_.name.equals(fromName))
-    val toOpt = addresses.find(_.name.equals(toName))
+    val fromOpt = byName.get(fromName)
+    val toOpt = byName.get(toName)
     for {
       from <- fromOpt
       to <- toOpt
+      if (from.size == 1 && to.size == 1)
     } yield {
-      val diff = to.birthDay.getTime - from.birthDay.getTime
+      val diff = to(0).birthDay.getTime - from(0).birthDay.getTime
       val timeUnit = TimeUnit.DAYS
       timeUnit.convert(diff, TimeUnit.MILLISECONDS)
     }
