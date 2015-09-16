e9c1ece013ef85ae7920339f4d3fec6f851d238b
Nicolas Juan Cavallo Fandino <nicolas.cavallo@picsolve.com>
2015-09-16 22:08:18 +0100

    Fixed code errors. 5

--- src/main/scala/gumtree/nicocavallo/GumtreeCodingChallenge.scala
+++ src/main/scala/gumtree/nicocavallo/GumtreeCodingChallenge.scala
@@ -50,9 +50,11 @@ trait GumtreeCodingChallenge {
     for {
       from <- fromOpt
       to <- toOpt
+      toHead <- to.headOption
+      fromHead <- from.headOption
       if from.size == 1 && to.size == 1
     } yield {
-      val diff = to.head.birthDay.getTime - from.head.birthDay.getTime
+      val diff = toHead.birthDay.getTime - fromHead.birthDay.getTime
       DAYS.convert(diff, MILLISECONDS)
     }
   }
